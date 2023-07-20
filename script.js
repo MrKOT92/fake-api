da9c3fae3e7accf35b4130fdef95d6591de21409
Cristian Pasat <mrkot9292@gmail.com>
2023-07-21 01:32:10 +0200

    fix bug

--- script.js
+++ script.js
@@ -3,37 +3,22 @@ const cardElement = document.getElementById("city-card");
  
 
 async function loadCity() {
-
   const res = await fetch("https://mrkot92.github.io/fake-api/kiev.json");
-
   const obj = await res.json();
-
   console.log(obj);
 
   const { name, settled, image } = obj;
 
-  // console.log(name);    // Kiev
-
-  // console.log(settled); // 6-7 век
-
-  // console.log(image);   // ссылка на фото 
-
-  const nameElement = document.createElement("span"); // <span></span>
-
-  nameElement.textContent = name;                     // <span>Kiev</span>
-
- 
+  const nameElement = document.createElement("span");
+  nameElement.textContent = name;
 
   const settledElement = document.createElement("span");
-
   settledElement.textContent = settled;
 
-  
+  const imageElement = document.createElement("img"); // Adăugați un element img pentru a afișa imaginea
+  imageElement.src = image; // Setează atributul src cu linkul imaginii
 
-  cardElement.append(nameElement, settledElement);
-
+  cardElement.append(nameElement, settledElement, imageElement);
 }
-
- 
 
 loadCity();
